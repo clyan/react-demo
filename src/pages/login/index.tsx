2250f85fb01b76fb66f74ffd97952209bf474391
ywymoshi <2429335889@qq.com>
2023-05-15 01:15:53 +0800

    chore: 更新eslint版本

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,7 +4,24 @@
  */
 
 import { useLocation, useNavigate } from 'react-router-dom'
+import React from 'react'
 import { useAuth } from '../auth/hooks/auth-provider'
+
+interface Props {
+  key?: string | number
+  enable?: boolean
+  zData?: string
+  onChange?: VoidFunction
+}
+function Hello(props: Props) {
+  return <div key={props.key} onClick={props.onChange} />
+}
+
+const hello = (
+  <div>
+    <p>Hello</p>
+  </div>
+)
 
 export default function Login() {
   const navigate = useNavigate()
@@ -27,14 +44,33 @@ export default function Login() {
   // eslint-disable-next-line no-console
   console.log('123')
 
+  const onChange = () => {
+    console.log('onChange')
+  }
+
   return (
     <div>
-      <p>You must log in to view the page at {from}</p>
+      <p>
+        You must log in to view the page at
+      </p>
       <form onSubmit={handleSubmit}>
         <label>
-          Username: <input name="username" type="text" />
-        </label>{' '}
-        <button type="submit">Login</button>
+          Username:
+          <input name="username" type="text" />
+        </label>
+        <button type="submit">
+          Login
+        </button>
+        <Hello enable zData="1" onChange={onChange} />
+        {!!from && (
+          <span>
+            {
+              [{ id: 1 }].map((thing, index) => (
+                <Hello key={index} />
+              ))
+            }
+          </span>
+        )}
       </form>
     </div>
   )
